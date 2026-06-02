f918c3b434d7bf2121bd935757a072759a281faf
eofdred <muratdogusan@gmail.com>
2026-06-02 21:57:19 +0300

    Quartz sync: Jun 2, 2026, 9:57 PM

---
+++ quartz/components/Body.tsx
@@ -0,0 +1,11 @@
+import {
+  QuartzComponent,
+  QuartzComponentConstructor,
+  QuartzComponentProps,
+} from "./types";
+
+const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
+  return <div id="quartz-body">{children}</div>;
+};
+
+export default (() => Body) satisfies QuartzComponentConstructor;
